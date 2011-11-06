7ab8055b0e8f33433ac2bd9a804b9c9a47887bf6
Jeff Dutil <jdutil21@gmail.com>
2011-11-06 12:56:52 -0500

    Fix undefined method first spec errors.

--- lib/deface/override.rb
+++ lib/deface/override.rb
@@ -336,14 +336,13 @@ module Deface
       virtual_path = details[:virtual_path]
       return [] if virtual_path.nil?
 
-      virtual_path = virtual_path[1..-1] if virtual_path.first == '/'
+      virtual_path = virtual_path[1..-1] if virtual_path[0] == '/'
 
       result = []
       result << self.all[virtual_path.to_sym].try(:values)
 
       result.flatten.compact.sort_by &:sequence
     end
-
 
     def self.all
       Rails.application.config.deface.overrides.all
