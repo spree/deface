5882cfc87d4ab4b92607565ed3c1905f366fd436
Elia Schito <elia@schito.me>
2020-07-20 19:44:39 +0200

    Add caching to bundler on Travis

--- lib/deface/action_view_extensions.rb
+++ lib/deface/action_view_extensions.rb
@@ -1,50 +1,66 @@
 module Deface::ActionViewExtensions
-  def initialize(source, identifier, handler, details)
-    syntax = Deface::ActionViewExtensions.determine_syntax(handler)
+  def self.determine_syntax(handler)
+    return unless Rails.application.config.deface.enabled
 
-    if syntax
-      processed_source = Deface::Override.apply(source.to_param, details, true, syntax)
-
-      # force change in handler before continuing to original Rails method
-      # as we've just converted some other template language into ERB!
-      #
-      if [:slim, :haml].include?(syntax) && processed_source != source.to_param
-        handler = ActionView::Template::Handlers::ERB
-      end
+    if handler.to_s == "Haml::Plugin"
+      :haml
+    elsif handler.class.to_s == "Slim::RailsTemplate"
+      :slim
+    elsif handler.to_s.demodulize == "ERB" || handler.class.to_s.demodulize == "ERB"
+      :erb
     else
-      processed_source = source.to_param
+      nil
+    end
+  end
+
+  module DefacedTemplate
+    def initialize(source, identifier, handler, details)
+      syntax = Deface::ActionViewExtensions.determine_syntax(handler)
+
+      if syntax
+        processed_source = Deface::Override.apply(source.to_param, details, true, syntax)
+
+        # force change in handler before continuing to original Rails method
+        # as we've just converted some other template language into ERB!
+        #
+        if [:slim, :haml].include?(syntax) && processed_source != source.to_param
+          handler = ActionView::Template::Handlers::ERB
+        end
+      else
+        processed_source = source.to_param
+      end
+
+      super(processed_source, identifier, handler, details)
     end
 
-    super(processed_source, identifier, handler, details)
-  end
+    # refresh view to get source again if
+    # view needs to be recompiled
+    #
+    def render(view, locals, buffer=nil, &block)
+      template_class = Deface.before_rails_6? ? ActionView::CompiledTemplates : ActionDispatch::DebugView
 
-  # refresh view to get source again if
-  # view needs to be recompiled
-  #
-  def render(view, locals, buffer=nil, &block)
-    template_class = Deface.before_rails_6? ? ActionView::CompiledTemplates : ActionDispatch::DebugView
+      mod = view.is_a?(template_class) ? template_class : view.singleton_class
 
-    mod = view.is_a?(template_class) ? template_class : view.singleton_class
+      if @compiled && !mod.instance_methods.include?(method_name.to_sym)
+        @compiled = false
+        @source = refresh(view).source
+      end
 
-    if @compiled && !mod.instance_methods.include?(method_name.to_sym)
-      @compiled = false
-      @source = refresh(view).source
+      super(view, locals, buffer, &block)
     end
 
-    super(view, locals, buffer, &block)
+    # inject deface hash into compiled view method name
+    # used to determine if recompilation is needed
+    #
+    def method_name
+      deface_hash = Deface::Override.digest(:virtual_path => @virtual_path)
+
+      #we digest the whole method name as if it gets too long there's problems
+      "_#{Deface::Digest.hexdigest("#{deface_hash}_#{super}")}"
+    end
+
+    ActionView::Template.prepend self
   end
-
-  # inject deface hash into compiled view method name
-  # used to determine if recompilation is needed
-  #
-  def method_name
-    deface_hash = Deface::Override.digest(:virtual_path => @virtual_path)
-
-    #we digest the whole method name as if it gets too long there's problems
-    "_#{Deface::Digest.hexdigest("#{deface_hash}_#{super}")}"
-  end
-
-  ActionView::Template.prepend self
 
   # Rails 6 fix.
   #
@@ -59,19 +75,5 @@ module Deface::ActionViewExtensions
     # We use include to place the module between the class' call to super and the
     # actual execution within Erubi::Engine.
     ActionView::Template::Handlers::ERB::Erubi.include self unless Deface.before_rails_6?
-  end
-
-  def self.determine_syntax(handler)
-    return unless Rails.application.config.deface.enabled
-
-    if handler.to_s == "Haml::Plugin"
-      :haml
-    elsif handler.class.to_s == "Slim::RailsTemplate"
-      :slim
-    elsif handler.to_s.demodulize == "ERB" || handler.class.to_s.demodulize == "ERB"
-      :erb
-    else
-      nil
-    end
   end
 end
