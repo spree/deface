4c2f3f76a2f6a60be6dde5f8b0fea551a8101e21
Elia Schito <elia@schito.me>
2021-02-12 15:27:32 +0100

    Cache the lookup context on the TemplateHandler, allow injection

--- lib/deface/template_helper.rb
+++ lib/deface/template_helper.rb
@@ -1,8 +1,13 @@
 module Deface
   module TemplateHelper
+    def self.lookup_context
+      @lookup_context ||= ActionView::LookupContext.new(
+        ActionController::Base.view_paths, {:formats => [:html]}
+      )
+    end
 
     # used to find source for a partial or template using virtual_path
-    def load_template_source(virtual_path, partial, apply_overrides=true)
+    def load_template_source(virtual_path, partial, apply_overrides=true, lookup_context: Deface::TemplateHelper.lookup_context)
       parts = virtual_path.split("/")
       prefix = []
       if parts.size == 2
@@ -15,10 +20,7 @@ module Deface
 
       #this needs to be reviewed for production mode, overrides not present
       Rails.application.config.deface.enabled = apply_overrides
-      @lookup_context ||= ActionView::LookupContext.new(ActionController::Base.view_paths, {:formats => [:html]})
-      view = @lookup_context.disable_cache do
-        @lookup_context.find(name, prefix, partial)
-      end
+      view = lookup_context.disable_cache { lookup_context.find(name, prefix, partial) }
 
       if view.handler.to_s == "Haml::Plugin"
         Deface::HamlConverter.new(view.source).result
