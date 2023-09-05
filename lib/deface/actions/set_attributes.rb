fce8b25eff60dfff86b5716c935ca0ccb3abac09
nepobo <nepobo-gl@yandex.ru>
2023-09-05 16:18:03 +0400

    minor changes

--- lib/deface/actions/set_attributes.rb
+++ lib/deface/actions/set_attributes.rb
@@ -1,7 +1,7 @@
 module Deface
   module Actions
     class SetAttributes < AttributeAction
-      def execute_for_attribute(target_element, name, value, assets: false)
+      def execute_for_attribute(target_element, name, value)
         target_element.remove_attribute(name)
         target_element.remove_attribute("data-erb-#{name}")
         target_element.set_attribute("data-erb-#{name}", value.to_s)
