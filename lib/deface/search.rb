21582ff8576cd3f172797e5e7bf3f0932b86ffb4
Elia Schito <elia@schito.me>
2025-07-31 10:33:24 +0200

    Optimize the search module
    - Freeze static objects
    - Avoid useless ops
    
    Inspired by #184

--- lib/deface/search.rb
+++ lib/deface/search.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 module Deface
   module Search
     module ClassMethods
+      VIRTUAL_PATH_CLEANUP_REGEXES = [
+        /^\//, # starting with a slash
+        /\.\w+\z/ # ending with a file extension
+      ].freeze
 
-      # finds all applicable overrides for supplied template
-      #
+      # Finds all applicable overrides for supplied template
       def find(details)
         return [] if self.all.empty? || details.empty?
 
         virtual_path = details[:virtual_path].dup
         return [] if virtual_path.nil?
 
-        [/^\//, /\.\w+\z/].each { |regex| virtual_path.gsub!(regex, '') }
+        VIRTUAL_PATH_CLEANUP_REGEXES.each { |regex| virtual_path.gsub!(regex, '') }
 
-        result = []
-        result << self.all[virtual_path.to_sym].try(:values)
-
-        result.flatten.compact.sort_by &:sequence
+        result = self.all[virtual_path.to_sym]&.values&.flatten || []
+        result.compact!
+        result.sort_by!(&:sequence)
+        result
       end
 
-      # finds all overrides that are using a template / parital as there source
-      #
+      # Finds all overrides that are using a template / parital as there source
       def find_using(virtual_path)
         self.all.map do |key, overrides_by_name|
           overrides_by_name.values.select do |override|
